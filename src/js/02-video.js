2f49a8b240f2ecd36d8f7c8dec4999792a7a91f9
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-11-15 11:30:42 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,23 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  };
+  
+  player.on('timeupdate', throttle(onPlay, 1000));
+  
+  player
+    .setCurrentTime(
+      JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+    )
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+  
+        default:
+          break;
+      }
+    });
